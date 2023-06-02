b13f00254b8404d22690a817145de9b9aae3011e
Eliza Ellis <elizaphoenixroseellis@gmail.com>
2023-06-02 16:06:09 +0100

    COMBO to excluded list. Error handling for new markets needs work

--- lib/discord.py
+++ lib/discord.py
@@ -8,16 +8,26 @@ conf = Config("config.yaml")
 def send_dictionary_to_channel(dictionary):
     # Build a formatted message from the dictionary
     lines = []
+    net_profit = 0  # Initialize net profit
     for symbol, entries in dictionary.items():
         lines.append(symbol)
         for entry, data in entries.items():
             entry_num = entry.split('_')[1]  # Extract entry number from 'entry_X'
-            side = data['side'].replace('🟩 🟩 🟩 BUY 🟩 🟩 🟩', 'BUY')  # Simplify side
+            side = data['side']  # Simplify side
             entry_price = round(data['entry_price'], 2)  # Round to 2 decimal places
             market_price = round(data['market_price'], 2)  # Round to 2 decimal places
             gain = round(data['percentage_gain'], 2)  # Round gain to 2 decimal places
-            line = f"    Entry {entry_num}: {side} at {entry_price}, current price {market_price}, gain {gain}%"
+            net_profit += gain  # Add gain to net profit
+            # Add emoji based on gain
+            emoji = '🟩' if gain > 0 else '🟥' if gain < 0 else '🟧'
+            line = f"    Entry {entry_num}: {side} at {entry_price}, current price {market_price}, gain {gain}% {emoji}"
             lines.append(line)
+
+    # Determine net profit emoji
+    net_profit_emoji = '🟩🟩🟩' if net_profit > 0 else '🟥🟥🟥' if net_profit < 0 else '🟧🟧🟧'
+    # Add a separating line and net profit line
+    lines.append("------------------------------------------------------------------------------------")  # Markdown for horizontal line
+    lines.append(f"Net Profit: {round(net_profit, 2)}% {net_profit_emoji}")
 
     # Combine all lines into a single string, with line breaks between lines
     message = "\n".join(lines)
@@ -32,6 +42,8 @@ def send_dictionary_to_channel(dictionary):
         result.raise_for_status()
     except requests.exceptions.HTTPError as err:
         print(err)
+
+
 
 
 
