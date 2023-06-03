9f7c938bf4b799b36652e77ac11453ebc0c7239f
Koola <koola@no-reply.com>
2023-06-03 20:00:58 +0800

    fix side

--- liquidation_acme.py
+++ liquidation_acme.py
@@ -114,7 +114,7 @@ async def process_message(msg) -> None:
                 if symbol not in trade_book:
                     trade_book[symbol] = {
                         "symbol": symbol,
-                        "side": msg["S"],
+                        "side": "SELL" if msg["S"] == "BUY" else "BUY",
                         "entry": float(scaled_close),
                         "ts": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'),
                         "close": 0,
